31900e04881821d3ad313d9ed0ca9b528e2a288a
Thiago <thiagofreitas1106@gmail.com>
2021-10-02 09:36:43 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -10,7 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-public class Category {
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
